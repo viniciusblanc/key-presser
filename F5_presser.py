28d029eaca19f3ed688928b8d4791d4c507f30eb
viniciusblanc <blanc.vinicius@gmail.com>
2022-01-14 13:48:55 -0300

    added hour

--- F5_presser.py
+++ F5_presser.py
@@ -5,14 +5,15 @@ def f5_presser():
     n = 0
     while 1:
         m, s = divmod(n, 60)
-        print('{:02}:{:02}'.format(m, s), end='\r')
+        h, m = divmod(m, 60)
+        print('{:02}:{:02}:{:02}'.format(h, m, s), end='\r')
         time.sleep(1)
         n += 1
         if s == 0 and m % 10 == 0:
             keyboard.press_and_release('F5')
 
 def main():
-    print('Refresher 1.2')
+    print('Refresher 1.3')
     f5_presser()
 
 if __name__ == '__main__':
